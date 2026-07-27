d402ed2c405c7597dcc914ae2f9fc1a744080f45
iroha924 <shunichi@hir4ta.com>
2026-07-27 09:04:56 +0900

    chore(release): v0.3.2

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -12,7 +12,7 @@ import { syncCommand } from "./commands/sync.js";
 import { muted, title } from "./render.js";
 import { renderUsage, renderValidationErrors, validationErrorsRendered } from "./usage.js";
 
-export const CLI_VERSION = "0.3.1";
+export const CLI_VERSION = "0.3.2";
 
 const mainCommand = define({
   name: "iroha",
